93cc639e090a98974e7aca8f54a06a71699deafc
Dorian <dovotori@protonmail.com>
2021-03-31 17:47:34 +0200

    fix white space

--- src/Lessons.jsx
+++ src/Lessons.jsx
@@ -149,33 +149,33 @@ const Expressions = () => {
         </span>
       </p>
       <p>
-        +い　だす <span className="grey">se mettre soudainement à</span>
+        +い だす <span className="grey">se mettre soudainement à</span>
       </p>
       <p>
-        +い　はじめる <span className="grey">commencer à</span>
+        +い はじめる <span className="grey">commencer à</span>
       </p>
       <p>
-        +い　にいく　/ にくる{" "}
+        +い にいく / にくる{" "}
         <span className="grey">vais / venir faire qq chose</span>
       </p>
       <p>
-        +い　すぎる <span className="grey">trop</span>
+        +い すぎる <span className="grey">trop</span>
       </p>
       <p>
-        nom + ちゅう　/ じゅう (中){" "}
+        nom + ちゅう / じゅう (中){" "}
         <span className="grey">pendant / partout</span>
       </p>
       <p>
-        +て　ください <span className="grey">imperatif poli</span>
+        +て ください <span className="grey">imperatif poli</span>
       </p>
       <p>
         +ない でください <span className="grey">imperatif negatif poli</span>
       </p>
       <p>
-        +い　たい <span className="grey">vouloir faire qq chose</span>
+        +い たい <span className="grey">vouloir faire qq chose</span>
       </p>
       <p>
-        +て　はいけません{" "}
+        +て はいけません{" "}
         <span className="grey">interdiction, impossibilité</span>
       </p>
       <p>
@@ -183,7 +183,7 @@ const Expressions = () => {
         <span className="grey">obligation</span>
       </p>
       <p>
-        +た　ほうがいい{" "}
+        +た ほうがいい{" "}
         <span className="grey">preferable de faire qq chose</span>
       </p>
       <p>
@@ -191,16 +191,16 @@ const Expressions = () => {
         <span className="grey">être capable de faire qq chose</span>
       </p>
       <p>
-        ＋る　まえに <span className="grey">avant avoir</span>
+        ＋る まえに <span className="grey">avant avoir</span>
       </p>
       <p>
-        +た　あとで <span className="grey">apres avoir</span>
+        +た あとで <span className="grey">apres avoir</span>
       </p>
       <p>
-        +て　もいいです <span className="grey">avoir la permission de</span>
+        +て もいいです <span className="grey">avoir la permission de</span>
       </p>
       <p>
-        +る　もいです <span className="grey">intention de faire qq chose</span>
+        +る もいです <span className="grey">intention de faire qq chose</span>
       </p>
     </Drawer>
   );
